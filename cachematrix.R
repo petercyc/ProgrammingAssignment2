87819b7ed4303122ab353c43d97b6fa7bf66e9da
petercyc <32408707+petercyc@users.noreply.github.com>
2017-10-15 20:25:46 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
 ## Matrix inversion is usually a costly computation and there may be some benefit 
 ## to caching the inverse of a matrix rather than compute it repeatedly (there 
 ## are also alternatives to matrix inversion that we will not discuss here). 
